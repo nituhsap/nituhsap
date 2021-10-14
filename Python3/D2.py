6d728247d62907cb08001399198886655fff3111
nituhsap <90912264+nituhsap@users.noreply.github.com>
2021-10-14 21:56:43 +0300

    Update D2.py

--- Python3/D2.py
+++ Python3/D2.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 if __name__ == "__main__":
     N = sys.argv
@@ -10,5 +11,17 @@ def fib(n):
         return M[n]
     M[n] = fib(n - 1) + fib(n - 2)
     return M[n]
+
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('N', type = int)
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = createParser()
+    namespace = parser.parse_args()
+    print (fib(namespace.N))
 
 fib(N)
