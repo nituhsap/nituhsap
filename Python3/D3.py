455007b6422984018bfb94fe96e0a53ea1690af3
nituhsap <90912264+nituhsap@users.noreply.github.com>
2021-10-14 21:58:04 +0300

    Update D3.py

--- Python3/D3.py
+++ Python3/D3.py
@@ -1,48 +1,27 @@
 import sys
 import argparse
 
-def aller():
-    parser = argparse.ArgumentParser()
-    parser.add_argument ('-a', '--all', nargs=0)
-
-    return parser
-
-
-def filer():
-    parser = argparse.ArgumentParser()
-    parser.add_argument ('-a', '--all')
-
-    return parser
-
-
-if __name__ == '__main__':
-    al = aller()
-    fil = filer()
-    ff = fil.parse_args(sys.argv[1:])
-    N = sys.argv
-
-M = {0: 0, 1: 1}
-
-def fib(n):
-    if n in M:
-        return M[n]
-    M[n] = fib(n - 1) + fib(n - 2)
+def fib(n, p = True):
+    fibs = []
+    x, y = 1, 1
+    for i in range(n):
+        z = x+y
+        x = y
+        y = z
+        fibs.append(str(x))
+    if p == True:
+        return ' '.join(fibs)
+    else:
+        return x
     return M[n]
 
-fib(N)
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('N', type = int)
+    parser.add_argument('-a', '--all', action='store_const', const=True)
+    return parser
 
-if ff == True:
-    if al == True:
-        with open(ff, "w") as out:
-            for i in range(1, N):
-                out.write(fib(i))
-    else:
-        with open(ff, "w") as out:
-            out.write(fib(N))
-
-else:
-    if al == True:
-        for i in range(1, N):
-            print(fib(i))
-    else:
-        print(fib(N))
+if __name__ == '__main__':
+    parser = createParser()
+    namespace = parser.parse_args()
+    print (fib(namespace.N, True))
